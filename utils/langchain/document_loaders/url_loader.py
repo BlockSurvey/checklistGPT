0600d8b021b54bc09e7767cfb928d24f9bb65aca
Raja Ilayaperumal <rajailayaperumal@Rajas-MacBook-Pro.local>
2023-08-14 16:29:54 +0530

    Memory leckage with bs4 is replaced with lxml package

--- utils/langchain/document_loaders/url_loader.py
+++ utils/langchain/document_loaders/url_loader.py
@@ -1,7 +1,7 @@
 from utils.langchain.document_loaders.document_loader_abc import DocumentLoaderInterface
-from bs4 import BeautifulSoup
-import gc
 import requests
+from lxml import html
+import re
 
 
 class UrlLoader(DocumentLoaderInterface):
@@ -16,18 +16,39 @@ class UrlLoader(DocumentLoaderInterface):
 
         # Check if the request was successful
         if response.status_code == 200:
-            # Parse the content using BeautifulSoup
-            soup = BeautifulSoup(response.content, 'lxml')
+            # # Parse the content using BeautifulSoup
+            # soup = BeautifulSoup(response.content, 'lxml')
 
-            # Extract only text content
-            text_content = soup.get_text(separator=' ', strip=True)
+            # # Extract only text content
+            # text_content = soup.get_text(separator=' ', strip=True)
 
-            # Clean up the soup object to free memory
-            soup.decompose()
-            del soup
-            gc.collect()
+            # # Clean up the soup object to free memory
+            # soup.decompose()
+            # del soup
+            # gc.collect()
 
-            return text_content
+            # return text_content
+
+            # # Parse the content using lxml
+            # tree = html.fromstring(response.content)
+
+            # # Extract the entire text content of the page
+            # return tree.text_content()
+            
+            # Parse the content using lxml
+            tree = html.fromstring(response.content)
+
+            # Remove <script>, <style>, and hidden elements from the tree
+            for to_remove in tree.xpath('//*[name()="script" or name()="style" or @hidden or @aria-hidden="true" or name()="input" and @type="hidden"]'):
+                to_remove.getparent().remove(to_remove)
+
+            # Extract the remaining text content of the page
+            raw_text = tree.text_content().strip()
+
+            # Replace sequences of whitespace characters with a single space
+            cleaned_text = re.sub(r'\s+', ' ', raw_text)
+
+            return cleaned_text
         else:
             raise ValueError(
                 f"Failed to retrieve content. HTTP Status Code: {response.status_code}")
