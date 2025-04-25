804a27d766369d2ed863586288fa6adec57d76b7
Joy-checklist <joy@checklist.gg>
2025-04-25 10:22:28 +0000

    Dependency Conflict Fix Change

--- controllers/checklist_controller.py
+++ controllers/checklist_controller.py
@@ -1,11 +1,12 @@
 from langchain.chains import LLMChain
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain_core.runnables import RunnableSequence
 from langchain_core.prompts.prompt import PromptTemplate
 from utils.langchain.langchain_utils import parse_agent_result_and_get_json
 
 
 class ChecklistController():
-    llm = OpenAI(temperature=0.5, model_name="gpt-3.5-turbo")
+    llm = ChatOpenAI(temperature=0.5, model="gpt-3.5-turbo")
 
     def __init__(self):
         pass
@@ -34,16 +35,18 @@ class ChecklistController():
         }
         ```"""
 
-        prompt_template = PromptTemplate(
-            input_variables=["final_prompt"], template=dynamic_template, partial_variables={"format_instructions": checklist_format_instructions})
+        # New PromptTemplate
+        prompt_template = PromptTemplate.from_template(dynamic_template)
 
-        checklist_chain = LLMChain(
-            llm=llm, prompt=prompt_template)
+        # Build the RunnableSequence chain
+        chain: RunnableSequence = prompt_template.partial(
+            format_instructions=checklist_format_instructions
+        ) | self.llm
 
-        result = checklist_chain.run(
-            {"final_prompt": prompt})
+        # Invoke the chain with the prompt
+        result = chain.invoke({"final_prompt": prompt})
 
-        return result
+        return result.content 
 
     def generate_checklist(self, prompt):
         # Null validation
